c1a429f998a6e696795c0f0fbe549b5caa389007
Mostafa-m-gomaa <aagomaaa2@gmail.com>
2025-05-06 01:05:26 +0300

    mostafa

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,17 +4,20 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AppContext } from '../App'
+import { LuLoader2 } from 'react-icons/lu'
 
 const Login = () => {
     const {setLogin} = useContext(AppContext)
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [loader,setLoader] = useState(false)
     const history = useNavigate()
 
     const onSubmit = async(e)=>{
+        setLoader(true)
         e.preventDefault()
         try{
-            const res = await fetch("https://api.theoriehaast.nl/api/v1/auth/login" , {
+            const res = await fetch("https://api.123theoriex.com/api/v1/auth/login" , {
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
@@ -25,6 +28,8 @@ const Login = () => {
                 })
             })
             const data = await res.json()
+            setLoader(false)
+            console.log(res)
             if(data.data){
                 sessionStorage.setItem("token" , data.data.token)
                 history("/home")
@@ -48,15 +53,15 @@ alert(data.message)
     <form action="" className="bg-white py-8 px-3 flex flex-col gap-6 sm:w-[45%] sm:px-40 sm:py-5" onSubmit={onSubmit}>
     <h1 className='text-[25px] text-center capitalize text-[#0D67B1] font-bold border-b-2 py-3 sm:text-left px-3'>welcome to . <span>rijinstructeurmakelijkworden  </span></h1>
         <div className="text-[35px] sm:text-[25px] font-bold capitalize">Inloggen</div>
-        <label htmlFor="" className='w-full flex flex-col gap-3 text-[18px] sm:text-[15px] sm:gap-1'>
+        <label  htmlFor="" className='w-full flex flex-col gap-3 text-[18px] sm:text-[15px] sm:gap-1'>
             Email
-            <input type="text " className='p-2 border-2 bg-[#E8F0FE]' onChange={(e)=>setEmail(e.target.value)} />
+            <input type="text" required className='p-2 border-2 bg-[#E8F0FE]' onChange={(e)=>setEmail(e.target.value)} />
         </label>
         <label htmlFor="" className='w-full flex flex-col gap-3 text-[18px]'>
         Wachtwoord
-            <input type="text " className='p-2 border-2 bg-[#E8F0FE]' onChange={(e)=>setPassword(e.target.value)} />
+            <input required type="text " className='p-2 border-2 bg-[#E8F0FE]' onChange={(e)=>setPassword(e.target.value)} />
         </label>
-        <button className="bg-[#FACA00] w-fit p-3 text-[20px] uppercase rounded-xl font-bold mx-auto hover:bg-[#0D67B1] transition-all" type='submit'>Inloggen</button>
+        <button className="bg-[#FACA00] w-fit p-3 text-[20px] uppercase rounded-xl font-bold mx-auto hover:bg-[#0D67B1] transition-all" type='submit' disabled={loader}>{loader ? <LuLoader2 className='animate-spin' />:"Inloggen"}</button>
 
     <div className="w-[80%] mx-auto">
 
